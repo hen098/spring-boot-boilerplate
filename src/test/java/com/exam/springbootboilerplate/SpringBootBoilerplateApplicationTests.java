fb3cdb62e54a99fc5ee8c55cfa608fc52722178f
hen <haeyon098@gmail.com>
2021-07-01 17:31:53 +0900

    first commit

---
+++ src/test/java/com/exam/springbootboilerplate/SpringBootBoilerplateApplicationTests.java
@@ -0,0 +1,13 @@
+package com.exam.springbootboilerplate;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class SpringBootBoilerplateApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
